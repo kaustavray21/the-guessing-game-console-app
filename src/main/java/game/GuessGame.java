1c0571dfdd0decaefabef3906e1f14209deed752
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-03-31 22:05:02 +0530

    created 3 variables to hold the guesses of the players

--- src/main/java/game/GuessGame.java
+++ src/main/java/game/GuessGame.java
@@ -6,11 +6,17 @@ public class GuessGame {
     Player playerTwo;
     Player playerThree;
 
-    // Create three player objects and assign them to three player instance variables.
     public void startGame() {
+        // Create three player objects and assign them to three player instance variables.
         playerOne = new Player();
         playerTwo = new Player();
         playerThree = new Player();
+
+        // Declare 3 variables to hold the 3 guesses that the Players make.
+        int guessPlayerOne = 0;
+        int guessPlayerTwo = 0;
+        int guessPlayerThree = 0;
+
     }
 }
 
