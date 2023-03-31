d5606964081fdc24fc3cdb8e9bac733dab1d2060
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-03-31 21:50:47 +0530

    created 3 player objects

--- src/main/java/game/GuessGame.java
+++ src/main/java/game/GuessGame.java
@@ -1,4 +1,8 @@
 package game;
 
 public class GuessGame {
+    // GuessGame has three instance variables for the three player objects.
+    Player playerOne;
+    Player playerTwo;
+    Player playerThree;
 }
