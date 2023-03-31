24ab977c990d74f44a0decfdf36c80b4fd4be87f
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-03-31 22:30:29 +0530

    created a method to guess the number

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -4,4 +4,9 @@ public class Player {
     // This instance variable stores the number that a player has guessed.
     int number;
 
+    // Create a method to guess the number.
+    public void guess() {
+        number = (int) (Math.random() * 10.0);
+    }
+
 }
