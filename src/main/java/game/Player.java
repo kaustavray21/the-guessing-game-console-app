247220f122773c75ead2a2e9dc869d3310539e64
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-03-31 22:42:16 +0530

    Created constructors for Player

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -4,9 +4,18 @@ public class Player {
     // This instance variable stores the number that a player has guessed.
     int number;
 
+    // create a parameterized constructor
+    public Player(int number) {
+        this.number = number;
+    }
+
+    // Create a no arguments constructor
+    public Player() {
+        this.number = 0;
+    }
+
     // Create a method to guess the number.
     public void guess() {
         number = (int) (Math.random() * 10.0);
     }
-
 }
